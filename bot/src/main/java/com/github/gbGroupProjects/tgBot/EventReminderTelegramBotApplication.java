ff475ed7d99e9cefca9d87c6aa8b1cb0b50c6084
Vladimir Runov <9322304@gmail.com>
2022-02-13 18:03:24 +0300

    initial commit

---
+++ bot/src/main/java/com/github/gbGroupProjects/tgBot/EventReminderTelegramBotApplication.java
@@ -0,0 +1,12 @@
+package com.github.gbGroupProjects.tgBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventReminderTelegramBotApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EventReminderTelegramBotApplication.class, args);
+    }
+}
