e80081acce968edf5186058a0d520f7b49786c35
Runov Vladimir <9322304@gmail.com>
2022-02-15 10:42:03 +0300

    +testdb  - UserDaoJdbc required a bean of type 'org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate' that could not be found.

---
+++ dao/src/test/java/com/github/gbGroupProjects/tgBot/dao/jdbc/CategoryDaoJdbcTest.java
@@ -0,0 +1,7 @@
+package com.github.gbGroupProjects.tgBot.dao.jdbc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryDaoJdbcTest {
+
+}
--- testdb/src/main/java/com/github/gbGroupProjects/tgBot/testdb/SpringJdbcConfig.java
+++ testdb/src/main/java/com/github/gbGroupProjects/tgBot/testdb/SpringJdbcConfig.java
@@ -1,4 +1,4 @@
-package com.github.gbGroupProjects.tgBot.testDb;
+package com.github.gbGroupProjects.tgBot.testdb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
