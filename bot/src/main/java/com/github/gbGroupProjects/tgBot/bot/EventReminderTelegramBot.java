1e6ba631f7c392c60f2277bb498b12d6ac6ffd7f
Runov Vladimir <9322304@gmail.com>
2022-02-16 20:32:44 +0300

    тест меню с кнопкой...

--- bot/src/main/java/com/github/gbGroupProjects/tgBot/bot/EventReminderTelegramBot.java
+++ bot/src/main/java/com/github/gbGroupProjects/tgBot/bot/EventReminderTelegramBot.java
@@ -8,10 +8,16 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Math.toIntExact;
 
 @Component
 public class EventReminderTelegramBot extends TelegramLongPollingBot {
@@ -42,8 +48,8 @@ public class EventReminderTelegramBot extends TelegramLongPollingBot {
         sendMessage(update, responseMessage);
     }
 
-    @Override
-    public void onUpdateReceived(Update update) {
+    //@Override
+    public void _onUpdateReceived(Update update) {
         long userTelegramId = update.getMessage().getFrom().getId();
         if (commandContainer.getUserCommand().isUserTelegramIdUnique(userTelegramId)) {  // 1915453131
             String sResult = commandContainer.getUserCommand().execute(update);
@@ -76,7 +82,57 @@ public class EventReminderTelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    @Override
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            if (update.getMessage().getText().equals("/start")) {
 
+                SendMessage message = new SendMessage(); // Create a message object object
+                message.setChatId(update.getMessage().getChatId().toString());
+                message.setText("You send /start");
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+                List<InlineKeyboardButton> rowInline = new ArrayList<>();
+                InlineKeyboardButton m = new InlineKeyboardButton();
+                m.setText("Update message text");
+                m.setCallbackData("update_msg_text");
+                rowInline.add(m);
+                // Set the keyboard to the markup
+                rowsInline.add(rowInline);
+                // Add it to the message
+                markupInline.setKeyboard(rowsInline);
+                message.setReplyMarkup(markupInline);
+                try {
+                    execute(message); // Sending our message object to user
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            } else {
+
+            }
+        } else if (update.hasCallbackQuery()) {
+            // Set variables
+            String call_data = update.getCallbackQuery().getData();
+            long message_id = update.getCallbackQuery().getMessage().getMessageId();
+            long chat_id = update.getCallbackQuery().getMessage().getChatId();
+
+            if (call_data.equals("update_msg_text")) {
+                String answer = "Updated message text";
+                EditMessageText new_message = new EditMessageText();
+                new_message.setChatId(update.getCallbackQuery().getMessage().getChatId().toString());
+                new_message.setMessageId(toIntExact(message_id));
+                new_message.setText(answer);
+                try {
+                    execute(new_message);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
     private void sendMessage(Update update, String message) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(update.getMessage().getChatId().toString());
