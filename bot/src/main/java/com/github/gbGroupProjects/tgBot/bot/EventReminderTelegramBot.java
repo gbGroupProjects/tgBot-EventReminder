8e30c5fb98de19ecadeef34c9cffbd59f55882c5
Runov Vladimir <9322304@gmail.com>
2022-02-16 21:15:39 +0300

    меню с кнопками.. список пользователей БД

--- bot/src/main/java/com/github/gbGroupProjects/tgBot/bot/EventReminderTelegramBot.java
+++ bot/src/main/java/com/github/gbGroupProjects/tgBot/bot/EventReminderTelegramBot.java
@@ -62,13 +62,7 @@ public class EventReminderTelegramBot extends TelegramLongPollingBot {
         }
 
         //User user = commandContainer.getUserByTelegramId(userTelegramId);
-        List<User> ls = commandContainer.getUserCommand().getAllUsers();
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("--users---\n");
-        for (User uu:ls ) {
-            stringBuilder.append("[" + uu.getUserId() + "],{" + uu.getTelegramId() + "}," + uu.getName() + "\n");
-        }
-        sendMessage(update, "" + stringBuilder.toString() + "" );
+        sendMessage(update, "" + getListUsers() + "" );
 
         if (update.hasMessage()) {
             if (update.getMessage().hasText()) {
@@ -86,24 +80,28 @@ public class EventReminderTelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
+            String telID = update.getMessage().getFrom().getId().toString();
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-            if (update.getMessage().getText().equals("/start")) {
-
+            if (true || update.getMessage().getText().equals("/start")) {
                 SendMessage message = new SendMessage(); // Create a message object object
                 message.setChatId(update.getMessage().getChatId().toString());
-                message.setText("You send /start");
+                message.setText(telID + "> You send /start");
                 InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
                 List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
                 List<InlineKeyboardButton> rowInline = new ArrayList<>();
-                    InlineKeyboardButton m = new InlineKeyboardButton();
-                    m.setText("Узнать события");
-                    m.setCallbackData("Действие_Кнопка_1");
+                InlineKeyboardButton m = new InlineKeyboardButton();
+                m.setText("Узнать события");
+                m.setCallbackData("Действие_Кнопка_1");
                 rowInline.add(m);
-                    InlineKeyboardButton m2 = new InlineKeyboardButton();
-                    m2.setText("Настройки");
-                    m2.setCallbackData("Действие_Кнопка_2");
+                InlineKeyboardButton m2 = new InlineKeyboardButton();
+                m2.setText("Настройки");
+                m2.setCallbackData("Действие_Кнопка_Настройки");
                 rowInline.add(m2);
+                InlineKeyboardButton m3 = new InlineKeyboardButton();
+                m3.setText("Удалить(ся)");
+                m3.setCallbackData("Действие_Кнопка_Удалиться");
+                rowInline.add(m3);
 
                 // Set the keyboard to the markup
                 rowsInline.add(rowInline);
@@ -125,18 +123,67 @@ public class EventReminderTelegramBot extends TelegramLongPollingBot {
             long chat_id = update.getCallbackQuery().getMessage().getChatId();
 
             if (call_data.equals("Действие_Кнопка_1")) {
-                String answer = "Событий на сегодня нет..";
+                String telID = update.getCallbackQuery().getFrom().getId().toString();
+                String answer = telID + "> Событий на сегодня нет..";
                 EditMessageText new_message = new EditMessageText();
                 new_message.setChatId(update.getCallbackQuery().getMessage().getChatId().toString());
                 new_message.setMessageId(toIntExact(message_id));
                 new_message.setText(answer);
+
+                InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+                List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+                List<InlineKeyboardButton> rowInline = new ArrayList<>();
+                InlineKeyboardButton m = new InlineKeyboardButton();
+                m.setText("Ok");
+                m.setCallbackData("Действие_Кнопка_OK");
+                rowInline.add(m);
+                InlineKeyboardButton m2 = new InlineKeyboardButton();
+                m2.setText("Не может быть!!");
+                m2.setCallbackData("Действие_Кнопка_НД");
+                rowInline.add(m2);
+
+                // Set the keyboard to the markup
+                rowsInline.add(rowInline);
+                // Add it to the message
+                markupInline.setKeyboard(rowsInline);
+                new_message.setReplyMarkup(markupInline);
+
                 try {
                     execute(new_message);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
+            } else if (call_data.equals("Действие_Кнопка_Настройки")) {
+                upSend(update, "settings", message_id);
+            } else if (call_data.equals("Действие_Кнопка_Удалиться")) {
+                upSend(update, "удаляемся....\n" + getListUsers() , message_id);
+            } else {
+                upSend(update, "def: ok!", message_id);
             }
         }
+    }
+
+    private void upSend(Update update, String text, long message_id) {
+        String telID = update.getCallbackQuery().getFrom().getId().toString();
+        String answer = telID + ">" +text;
+        EditMessageText new_message = new EditMessageText();
+        new_message.setChatId(update.getCallbackQuery().getMessage().getChatId().toString());
+        new_message.setMessageId(toIntExact(message_id));
+        new_message.setText(answer);
+        try {
+            execute(new_message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    private String getListUsers() {
+        List<User> ls = commandContainer.getUserCommand().getAllUsers();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("--users---\n");
+        for (User uu:ls ) {
+            stringBuilder.append("[" + uu.getUserId() + "],{" + uu.getTelegramId() + "}," + uu.getName() + "\n");
+        }
+        return stringBuilder.toString() + "";
     }
     private void sendMessage(Update update, String message) {
         SendMessage sendMessage = new SendMessage();
