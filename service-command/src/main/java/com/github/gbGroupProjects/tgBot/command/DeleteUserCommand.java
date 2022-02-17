c6f028e5d32b01e97b8c5fcfa1e5a99a4da5bf08
Riza05 <riza_misrikhanov05@mail.ru>
2022-02-17 10:53:02 +0300

    new add class DeleteCommand

--- service-command/src/main/java/com/github/gbGroupProjects/tgBot/command/DeleteUserCommand.java
+++ service-command/src/main/java/com/github/gbGroupProjects/tgBot/command/DeleteUserCommand.java
@@ -2,11 +2,11 @@ package com.github.gbGroupProjects.tgBot.command;
 
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class DeleteCommand implements Command{
-    private final String DELETE_MESSAGE = "You can delete your events";
+public class DeleteUserCommand implements Command{
+    private final String DELETE_USER_MESSAGE = "You can delete your events";
 
     @Override
     public String execute(Update update) {
-        return DELETE_MESSAGE;
+        return DELETE_USER_MESSAGE;
     }
 }
